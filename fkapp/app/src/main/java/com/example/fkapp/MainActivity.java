22c91df1866bb3a9cde3d064ddc741532eba56ba
pzp <1584391445@qq.com>
2021-11-26 20:42:53 +0800

    sql模块（未实装）

--- fkapp/app/src/main/java/com/example/fkapp/MainActivity.java
+++ fkapp/app/src/main/java/com/example/fkapp/MainActivity.java
@@ -3,26 +3,74 @@ package com.example.fkapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.mysql.jdbc.Statement;
+
+import org.xutils.common.Callback;
+import org.xutils.common.util.LogUtil;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mBtnEditText;
-    private Button mBtList1;
-    private Button mBtBts;
+    private Button mBtsql;
+    private TextView tv_show;
+    private ImageView img_show;
+    private TextView tv_sqlshow;
+    private Button bt_show;
 
+    //在主线程中更新UI
+    @SuppressLint("HandlerLeak")
+    private final Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.what){
+                case 0x12:
+                    String s = (String) msg.obj;
+                    tv_sqlshow.setText(s); //显示数据库中的数据
+                    break;
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBtnEditText=(Button)findViewById(R.id.btn_exittext);
+        Button mBtnEditText = (Button) findViewById(R.id.btn_exittext);
         mBtnEditText.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -31,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        mBtList1=(Button)findViewById(R.id.btn_list1);
+        Button mBtList1 = (Button) findViewById(R.id.btn_list1);
         mBtList1.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -41,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        mBtBts= (Button) findViewById(R.id.btn_BtsReg);
+        Button mBtBts = (Button) findViewById(R.id.btn_BtsReg);
         mBtBts.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -63,5 +111,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        mBtsql = (Button) findViewById(R.id.btn_mysql);
+        mBtsql.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //跳转到返校登记填写界面
+                Intent intent = new Intent(MainActivity.this,MysqlTest.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
+
 }
